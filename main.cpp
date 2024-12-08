a0b670f4e141ca9bdb5dd742b456dfd1d51d283c
Kamil <kgruca7747@gmail.com>
2024-12-08 14:23:56 +0100

    Aktualizacja funkcji main

--- main.cpp
+++ main.cpp
@@ -3,7 +3,18 @@
 
 int main()
 {
-    
+    int przedzialy, watki;
+
+	std::cout << "Podaj liczbe przedzialow: ";
+	std::cin >> przedzialy;
+	std::cout << "Podaj liczbe watkow: ";
+	std::cin >> watki;
+
+	Integral calka(przedzialy, watki);
+	calka.oblicz();
+
+	std::cout << "Przyblizona wartosc liczby pi: " << calka.pobierz_wynik_pi() << std::endl;
+	std::cout << "Czas obliczen: " << calka.pobierz_czas_obliczen() << " sekund" << std::endl;
 }
 
 
