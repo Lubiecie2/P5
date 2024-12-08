a91dcbdae1f2b05ce4a5bdfcf5df712b58264107
Kamil <kgruca7747@gmail.com>
2024-12-08 18:02:49 +0100

    Dodanie komentarzy

--- integral.cpp
+++ integral.cpp
@@ -1,63 +1,68 @@
-#include "integral.h"
-#include <iostream>
-#include <thread>
-#include <vector>
-#include <cmath>
+#include "integral.h"  
+#include <iostream>  
+#include <thread> // <--- Biblioteka do obslugi watkow
+#include <vector> // <--- Biblioteka do obslugi wektorow
+#include <cmath>  // <--- Biblioteka do obliczen matematycznych
 
 Integral::Integral(int intervals, int threads)  // <--- Konstruktor klasy	
 {
-	liczba_przedzialow = intervals;  // <--- Liczba przydzia³ów
-	liczba_watkow = threads;         // <--- Liczba w¹tków
-	wynik_pi = 0;  		             // <--- Wynik obliczeñ liczby pi
-	czas_obliczen = 0;               // <--- Czas trwania obliczeñ 
+	liczba_przedzialow = intervals;  // <--- Liczba przydzia³u ca³kowania
+	liczba_watkow = threads;         // <--- Liczba watkow do wykorzystania
+	wynik_pi = 0;  		             // <--- Wynik obliczen liczby pi
+	czas_obliczen = 0;               // <--- Czas trwania obliczen 
 }
 
-double Integral::calculatre_partial(int start, int end)  // <--- Funkcja obliczaj¹ca wartoœæ liczby pi
-{
-	double dx = 1.0 / liczba_przedzialow;  // <--- D³ugoœæ przedzia³u
-	double sum = 0;                        // <--- Suma wartoœci funkcji
-	for (int i = start; i < end; i++)      // <--- Pêtla obliczaj¹ca wartoœæ funkcji
+double Integral::calculatre_partial(int start, int end)  // <--- Funkcja obliczajaca czesc calki dla zakresu przedzialu przez parametry
+{                                                        //      start i end
+	double dx = 1.0 / liczba_przedzialow;  // <--- Dlugosc jednego podprzedzialu
+	double sum = 0;                        // <--- Inicjalizacja sumy
+	for (int i = start; i < end; i++)      // <--- Petla do iteracji po przydzielonym przedziale
 	{
-		double x = (i + 0.5) * dx;         // <--- Wartoœæ x
-		sum += 4.0 / (1.0 + x * x);        // <--- Wartoœæ funkcji
+		double x = (i + 0.5) * dx;         // <--- Obliczanie srodka przedzia³u
+		sum += 4.0 / (1.0 + x * x);        // <--- Obliczanie wartosci funkcji 4/(1+x^2) w punkcie x
 	}
-	return sum;                            // <--- Zwrócenie wartoœci funkcji
+	return sum;                            // <--- Zwrocenie wartosci funkcji
 }
 
-void Integral::oblicz()
+void Integral::oblicz()  // <--- Funkcja wykonujaca glowne obliczenia rownloegle przy uzyciu wielu watkow
 {
-	auto start = std::chrono::high_resolution_clock::now();
-	std::vector<std::thread> threads;
-	std::vector<double> results(liczba_watkow);
-	for (int i = 0; i < liczba_watkow; i++)
-	{
+	auto start = std::chrono::high_resolution_clock::now();  // <--- Pomiar czasu rozpoczecia obliczen
+	std::vector<std::thread> threads;   // <--- Wektor do przechowywania obiektow typu thread, kazdy
+										//      obiekt odpowiada za jeden watek						
+	std::vector<double> results(liczba_watkow);  // <--- Wektor do przechowywania wynikow czesciowych dla kazdego watku
+
+	for (int i = 0; i < liczba_watkow; i++)                  // <--- Petla do tworzenia watkow, kazdy watek
+	{                                                        //      oblicza czesc calki czesci calki w zakresie
 		int start = i * liczba_przedzialow / liczba_watkow;
 		int end = (i + 1) * liczba_przedzialow / liczba_watkow;
-		threads.push_back(std::thread([this, start, end, i, &results]()
-			{
-				results[i] = calculatre_partial(start, end);
-			}));
-	}
-	for (auto& t : threads)
-	{
-		t.join();
-	}
-	wynik_pi = 0;
-	for (int i = 0; i < liczba_watkow; i++)
-	{
-		wynik_pi += results[i];
-	}
-	wynik_pi *= 1.0 / liczba_przedzialow;
-	auto end = std::chrono::high_resolution_clock::now();
-	czas_obliczen = std::chrono::duration<double>(end - start).count();
-}
 
-double Integral::pobierz_wynik_pi()
+		threads.push_back(std::thread([this, start, end, i, &results]()  // <--- Tworzenie jest nowy watek 
+			{                                                            // this przekazuje obiekt klasy Integral do watku
+				results[i] = calculatre_partial(start, end);    // <--- kazdy watek wywoluje funkcje calculatre_partial
+			}));                                                //      i zapisuje wynik w wektorze results
+	}
+	for (auto& t : threads)  // <--- Petla przechodzi przez wszystkie watki i czeka na zakonczenie ich dzialania
+	{
+		t.join();  // <--- Dla kazdego watku wywoluje funkcje join, ktora blokuje glowny watek programu
+	}              //      do czasu zakonczenia dzialania podrzednego watku
+
+	wynik_pi = 0;   // <--- Inicjalizacja wyniku
+	for (int i = 0; i < liczba_watkow; i++)   // <--- Petla sumuje wyniki czesciowe z kazdego watku
+	{
+		wynik_pi += results[i]; // <--- Sumowanie wynikow czesciowych
+	}
+	wynik_pi *= 1.0 / liczba_przedzialow;  // <--- Wynik koncowy jest mnozony przez dlugosc przedzialu
+
+	auto end = std::chrono::high_resolution_clock::now();  // <--- Pomiar czasu zakonczenia obliczen
+	czas_obliczen = std::chrono::duration<double>(end - start).count(); // <--- Roznica czasu pomiedzy startem i koncem 
+}                                                                       //      to czas trwania obliczen
+
+double Integral::pobierz_wynik_pi()  // <--- Funkcja zwracajaca wynik obliczen liczby pi
 {
 	return wynik_pi;
 }
 
-double Integral::pobierz_czas_obliczen()
+double Integral::pobierz_czas_obliczen()   // <--- Funkcja zwracajaca czas trwania obliczen
 {
 	return czas_obliczen;
 }
